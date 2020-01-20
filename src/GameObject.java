e36e9d3f128281f21fade4c2cee692e693cd7657
marcus-elia <mselia@uvm.edu>
2020-01-20 16:02:39 -0500

    The GameObject class is now an abstract class that has tick(), render(), and the 4 move(amount) functions declared.

---
+++ src/GameObject.java
@@ -0,0 +1,14 @@
+import java.awt.*;
+
+public abstract class GameObject
+{
+    public GameObject(){};
+
+    public abstract void tick();
+    public abstract void render(Graphics2D g2d);
+
+    public abstract void moveDown(double amount);
+    public abstract void moveUp(double amount);
+    public abstract void moveLeft(double amount);
+    public abstract void moveRight(double amount);
+}
