3c30dd06eff72a7d224a6e05ef216eb1846c0ed7
marcus-elia <mselia@uvm.edu>
2020-01-20 16:39:16 -0500

    Created the Enforcer class, which extends MovableCircle and has functions to check if it is too close to an edge (this will cause everything else to scroll).

---
+++ src/Enforcer.java
@@ -0,0 +1,117 @@
+import java.awt.*;
+
+public class Enforcer extends MovableCircle
+{
+    private KeyDirection movementDirection;
+    private int edgeCushion;  // How close the player has to be to the wall to scroll instead of moving.
+
+    public Enforcer(GameManager inputManager,
+                    Point inputCenter, double inputRadius, double inputAngle, double inputCurSpeed,
+                    double inputMaxSpeed, double inputAcceleration, double inputVx, double inputVy,
+                    int inputEdgeCushion)
+    {
+        super(inputManager, inputCenter, inputRadius, inputAngle, inputCurSpeed, inputMaxSpeed,
+                inputAcceleration, inputVx, inputVy);
+        movementDirection = KeyDirection.None;
+        edgeCushion = inputEdgeCushion;
+    }
+
+    @Override
+    public void tick() {
+
+    }
+
+    @Override
+    public void render(Graphics2D g2d)
+    {
+        g2d.setColor(Color.BLUE);
+        g2d.fill(circle);
+    }
+
+    // ------------------------------------------
+    // ==========================================
+    //
+    //                 Getters
+    //
+    // ==========================================
+    // ------------------------------------------
+
+    public KeyDirection getMovementDirection()
+    {
+        return movementDirection;
+    }
+    public int getEdgeCushion()
+    {
+        return edgeCushion;
+    }
+
+    // ------------------------------------------
+    // ==========================================
+    //
+    //                 Setters
+    //
+    // ==========================================
+    // ------------------------------------------
+
+    public void setMovementDirection(KeyDirection input)
+    {
+        movementDirection = input;
+    }
+    public void setEdgeCushion(int input)
+    {
+        edgeCushion = input;
+    }
+
+
+    // ------------------------------------------
+    // ==========================================
+    //
+    //            Movement Functions
+    //
+    // ==========================================
+    // ------------------------------------------
+
+
+    // These functions check if the Enforcer is too close to an edge. If so, they move it
+    // and return the amount it was moved.
+    public double checkAndCorrectLeft()
+    {
+        double overlap = radius + edgeCushion - center.x;
+        if(overlap > 0)
+        {
+            center.x += overlap;
+            return overlap;
+        }
+        return 0;
+    }
+    public double checkAndCorrectRight()
+    {
+        double overlap = center.x - (manager.getWidth() - radius - edgeCushion);
+        if(overlap > 0)
+        {
+            center.x -= overlap;
+            return overlap;
+        }
+        return 0;
+    }
+    public double checkAndCorrectTop()
+    {
+        double overlap = radius + edgeCushion - center.y;
+        if(overlap > 0)
+        {
+            center.y += overlap;
+            return overlap;
+        }
+        return 0;
+    }
+    public double checkAndCorrectBottom()
+    {
+        double overlap = center.y - (manager.getHeight() - radius - edgeCushion);
+        if(overlap > 0)
+        {
+            center.y -= overlap;
+            return overlap;
+        }
+        return 0;
+    }
+}
