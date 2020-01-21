d9236760f9a4e45f1ea30cd804c01ad15e31b8d1
marcus-elia <mselia@uvm.edu>
2020-01-21 08:45:03 -0500

    Copied in the KeyInput class from random city, and updated the name of KeyDirection from ScrollDirection.

---
+++ src/KeyInput.java
@@ -0,0 +1,135 @@
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class KeyInput extends KeyAdapter
+{
+    private GameManager manager;
+
+    // Is the user holding down an arrow key?
+    private boolean leftArrowKey;
+    private boolean rightArrowKey;
+    private boolean upArrowKey;
+    private boolean downArrowKey;
+
+    public KeyInput(GameManager inputManager)
+    {
+        manager = inputManager;
+
+        leftArrowKey = false;
+        rightArrowKey = false;
+        upArrowKey = false;
+        downArrowKey = false;
+    }
+
+    public void keyPressed(KeyEvent e)
+    {
+        int key = e.getKeyCode();
+
+        if(key == KeyEvent.VK_LEFT)
+        {
+            leftArrowKey = true;
+            setManagerKeyDirection();
+        }
+        if(key == KeyEvent.VK_RIGHT)
+        {
+            rightArrowKey = true;
+            setManagerKeyDirection();
+        }
+        if(key == KeyEvent.VK_UP)
+        {
+            upArrowKey = true;
+            setManagerKeyDirection();
+        }
+        if(key == KeyEvent.VK_DOWN)
+        {
+            downArrowKey = true;
+            setManagerKeyDirection();
+        }
+    }
+
+    public void keyReleased(KeyEvent e)
+    {
+        int key = e.getKeyCode();
+
+        if(key == KeyEvent.VK_A)
+        {
+            leftArrowKey = false;
+            setManagerKeyDirection();
+        }
+        if(key == KeyEvent.VK_D)
+        {
+            rightArrowKey = false;
+            setManagerKeyDirection();
+        }
+        if(key == KeyEvent.VK_W)
+        {
+            upArrowKey = false;
+            setManagerKeyDirection();
+        }
+        if(key == KeyEvent.VK_S)
+        {
+            downArrowKey = false;
+            setManagerKeyDirection();
+        }
+    }
+
+
+    public void setManagerKeyDirection()
+    {
+        if(upArrowKey || downArrowKey || leftArrowKey || rightArrowKey)
+        {
+            // Up
+            if((upArrowKey && !leftArrowKey && !downArrowKey && !rightArrowKey) ||
+                    (upArrowKey && leftArrowKey && !downArrowKey && rightArrowKey))
+            {
+                manager.setCurrentKeyDirection(KeyDirection.Up);
+            }
+            // Down
+            else if((!upArrowKey && !leftArrowKey && downArrowKey && !rightArrowKey) ||
+                    (!upArrowKey && leftArrowKey && downArrowKey && rightArrowKey))
+            {
+                manager.setCurrentKeyDirection(KeyDirection.Down);
+            }
+            // Left
+            else if((!upArrowKey && leftArrowKey && !downArrowKey && !rightArrowKey) ||
+                    (upArrowKey && leftArrowKey && downArrowKey && !rightArrowKey))
+            {
+                manager.setCurrentKeyDirection(KeyDirection.Left);
+            }
+            // Right
+            else if((!upArrowKey && !leftArrowKey && !downArrowKey && rightArrowKey) ||
+                    (upArrowKey && !leftArrowKey && downArrowKey && rightArrowKey))
+            {
+                manager.setCurrentKeyDirection(KeyDirection.Right);
+            }
+            // UpLeft
+            else if(upArrowKey && leftArrowKey && !downArrowKey) // don't need to check right
+            {
+                manager.setCurrentKeyDirection(KeyDirection.UpLeft);
+            }
+            // DownLeft
+            else if(!upArrowKey && leftArrowKey && downArrowKey) // don't need to check right
+            {
+                manager.setCurrentKeyDirection(KeyDirection.DownLeft);
+            }
+            // DownRight
+            else if(!upArrowKey && !leftArrowKey && downArrowKey) // don't need to check right
+            {
+                manager.setCurrentKeyDirection(KeyDirection.DownRight);
+            }
+            // UpRight
+            else if(upArrowKey && !leftArrowKey && !downArrowKey) // don't need to check right
+            {
+                manager.setCurrentKeyDirection(KeyDirection.UpRight);
+            }
+            else
+            {
+                manager.setCurrentKeyDirection(KeyDirection.None);
+            }
+        }
+        else
+        {
+            manager.setCurrentKeyDirection(KeyDirection.None);
+        }
+    }
+}
