f26ec3af25b623762c365d758715a7cd2b4428ac
marcus-elia <mselia@uvm.edu>
2020-01-21 12:35:48 -0500

    Added removing function declarations to the GameObject class, so the manager can iterate through all GameObjects and remove things.

--- src/GameObject.java
+++ src/GameObject.java
@@ -7,8 +7,12 @@ public abstract class GameObject
     public abstract void tick();
     public abstract void render(Graphics2D g2d);
 
+    public abstract boolean getNeedsToBeRemoved();
+
     public abstract void moveDown(double amount);
     public abstract void moveUp(double amount);
     public abstract void moveLeft(double amount);
     public abstract void moveRight(double amount);
+
+    public abstract void removeSelf();
 }
